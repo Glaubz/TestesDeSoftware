80e76c2f3d6c2f4ee0db5d832a26ecbcf2200366
Matheus Glauber de Araujo Ribeiro <matheus.araujo@softwillians.com.br>
2023-02-13 20:49:19 -0300

    Arquivos de ambiente de teste

---
+++ Demo.Tests/Fixtures/NumerosParaCalculoFixture.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.Tests.Fixtures
+{
+    public class NumerosParaCalculoFixture
+    {
+
+    }
+}
--- Demo.Tests/UnitTest1.cs
+++ Demo.Tests/UnitTest1.cs
@@ -2,19 +2,64 @@ using Xunit;
 
 namespace Demo.Tests
 {
+    public class NumerosParaCalculo 
+    {
+        public NumerosParaCalculo() { }
+
+        public NumerosParaCalculo(int intA, int intB) 
+        { 
+            InteiroA = intA;
+            InteiroB = intB;
+        }
+
+        public int InteiroA { get; set; }
+        public int InteiroB { get; set; }
+        public decimal DecimalA { get; set; }
+        public decimal DecimalB { get; set; }
+    }
+
     public class UnitTest1
     {
-        [Fact]
+        public NumerosParaCalculo NumerosParaCalculo()
+        {
+            var numeros = new NumerosParaCalculo(
+                intA: 2,
+                intB: 2);
+
+            return numeros;
+        }
+
+        [Fact(DisplayName = "VerificarSoma")]
+        [Trait("Teste","Soma")]
         public void Calculadora_Somar_VerificarSoma()
         {
             //Arrange
             Calculadora _calculadora = new Calculadora();
 
             //Act
-            double _resultado = _calculadora.Somar(2,4);
+            double _resultado = _calculadora.Somar(2,2);
 
             //Assert
             Assert.Equal(4, _resultado);
+        }
+
+        [Theory(DisplayName = "Verificar Somas")]
+        [Trait("Teste","Somas")]
+        [InlineData(2,2,4)]
+        [InlineData(77,23,100)]
+        [InlineData(22,22,44)]
+        [InlineData(2456,1000,3456)]
+        [InlineData(1,2,3)]
+        public void Calculadora_Somar_VerificarSomas(double a, double b, double total)
+        {
+            //Arrange
+            Calculadora _calculadora = new Calculadora();
+
+            //Act
+            double _resultado = _calculadora.Somar(a, b);
+
+            //Assert
+            Assert.Equal(_resultado, total);
         }
     }
 }
